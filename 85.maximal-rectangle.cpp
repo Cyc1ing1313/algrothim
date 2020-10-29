f3a1d4bbd741bdeb58adb6298842cbcf5fa0a2a0
zhangkangzhe <zhangkangzhe@xiaomi.com>
2020-10-29 13:51:48 +0800

    2020-10-29 打卡

--- 85.maximal-rectangle.cpp
+++ 85.maximal-rectangle.cpp
@@ -6,19 +6,40 @@
 
 // @lc code=start
 #include<vector>
+#include<stack>
 using namespace std;
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int m = matrix.size();
+        if(m==0) return 0;
         int n = matrix[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> nums(n+2,0);
+        int ret=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j] = dp[i-1][]
+                if(matrix[i][j]=='1') nums[j+1] +=1;
+                if(matrix[i][j]=='0') nums[j+1] = 0;
             }
+            ret = max(ret,largestRectangleArea(nums));
         }
+        return ret;
 
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        int ret=0;
+        for(int i=0;i<n;i++){
+            while(!st.empty()&&heights[i]<heights[st.top()]){
+                int hight = heights[st.top()];st.pop();
+                int left = st.top();
+                ret = max(ret,hight*(i-left-1));
+            }
+            st.push(i);
+        }
+        return ret;
     }
 };
 // @lc code=end
