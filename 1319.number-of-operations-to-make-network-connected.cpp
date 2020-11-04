4bbaa14b144029372a3230ef10034bc50053e626
zhangkangzhe <zhangkangzhe@xiaomi.com>
2020-11-04 21:39:27 +0800

    2020-11-04 打卡

--- 1319.number-of-operations-to-make-network-connected.cpp
+++ 1319.number-of-operations-to-make-network-connected.cpp
@@ -12,19 +12,24 @@ public:
     vector<int> fa;
     int find(int x){
         if(fa[x]=x) return x;
-        fa[x] = find(x);
+        fa[x] = find(fa[x]);
         return fa[x];
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size()<n-1) return -1;
         fa.resize(n);
-        iota(fa.begin(),fa.end(),0);
-        int ret = n;
+        for(int i=0;i<n;i++){
+            fa[i]=i;
+        }
+        int ret=n;
         for(auto c : connections){
             int p = find(c[0]),q=find(c[1]);
             if(p!=q){
-                
+                fa[p]=fa[q];
+                ret -=1;
             }
         }
+        return ret-1;
     }
 };
 // @lc code=end
